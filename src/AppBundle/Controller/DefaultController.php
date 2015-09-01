6695f84f1e1337a37d176f495f761b41fb808467
Pavel Rozehnal <rozehnal@gmail.com>
2015-09-01 23:47:18 +0200

    simple test

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,9 @@
 
 namespace AppBundle\Controller;
 
+use Paro\MailerBundle\Model\File\FileConsumer;
+use Paro\MailerBundle\Model\File\FileProducer;
+use Paro\MailerBundle\Model\Message;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,6 +16,21 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+
+
+        $message = new Message();
+        $message->setTo('tester@tester.cz');
+        $message->setPlainContent('plain_text');
+
+        $folder = '/Applications/XAMPP/htdocs/mailer-bundle/Tests/data';
+
+        $producer = new FileProducer($folder);
+        $producer->add($message);
+
+        $consumer = new FileConsumer($folder);
+        $messageRecieved = $consumer->get();
+
+        var_dump($messageRecieved);
 
 
         // replace this example code with whatever you need
